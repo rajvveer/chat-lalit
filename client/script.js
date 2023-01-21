c28f004c3b27a4498c52423fe1ff435ca3038a50
Rajveer(Chiranjeev)Shekhawat <121601167+rajvveer@users.noreply.github.com>
2023-01-21 11:05:34 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localh ost:5000', {
+    const response = await fetch('https://chatcpt9.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
